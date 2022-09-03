c6b5c32bd7dd1c8f607ef85917e588addc68ce0d
Cintrian <velegoj2@yandex.ru>
2022-09-03 21:52:09 +0300

    Task6

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 5
-        byte paintCans = 120;
-        int classes = paintCans / 6;
-        int whitePaintCans = classes * 2;
-        int brownPaintCans = classes * 4;
-        System.out.println("В школе, где " + classes + " классов, нужно " + whitePaintCans + " банок белой краски и " + brownPaintCans + " банок коричневой краски");
+        // Задача 6
+        byte bananaWeight = 80;
+        byte milk100MlWeight = 105;
+        byte iceCreamWeight = 100;
+        byte eggWeight = 70;
+        double productWeightGram = bananaWeight * 5 + milk100MlWeight * 2 + iceCreamWeight * 2 + eggWeight * 4;
+        double productWeightKilo = productWeightGram / 1000;
+        System.out.println("Вес спорт-завтрака " + productWeightKilo + " килограмм");
         }
 }
