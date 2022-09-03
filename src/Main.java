5c1b14d344ff00db2f87cb3c5824509cac2eaab2
Cintrian <velegoj2@yandex.ru>
2022-09-03 20:12:48 +0300

    Task1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int ant = 1234567;
+        System.out.println("Значение переменной ant с типом int равно " + ant);
+        byte ball = 50;
+        System.out.println("Значение переменной ball с типом byte равно " + ball);
+        short cannedFood = 15300;
+        System.out.println("Значение переменной cannedFood с типом short равно " + cannedFood);
+        long minute = 987654321L;
+        System.out.println("Значение переменной minute с типом long равно " + minute);
+        float bugWeight = 0.153f;
+        System.out.println("Значение переменной bugWeight с типом float равно " + bugWeight);
+        double bagWeight = 5.14567315;
+        System.out.println("Значение переменной bagWeight с типом double равно " + bagWeight);
+        }
+}
