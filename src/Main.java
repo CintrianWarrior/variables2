c369233bf5cf44ffbf6b0f620cff28aed3a2a642
Cintrian <velegoj2@yandex.ru>
2022-09-03 22:37:12 +0300

    Task7

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 6
-        byte bananaWeight = 80;
-        byte milk100MlWeight = 105;
-        byte iceCreamWeight = 100;
-        byte eggWeight = 70;
-        double productWeightGram = bananaWeight * 5 + milk100MlWeight * 2 + iceCreamWeight * 2 + eggWeight * 4;
-        double productWeightKilo = productWeightGram / 1000;
-        System.out.println("Вес спорт-завтрака " + productWeightKilo + " килограмм");
+        // Задача 7
+        short necessaryWeightLoss = 7000;
+        short firstWeightLossPerDay = 250;
+        short secondWeightLossPerDay = 500;
+        int daysAtFirstVariant = necessaryWeightLoss / firstWeightLossPerDay;
+        System.out.println("Если спортсмен будет терять за день по 250 грамм, то для похудения ему потребуется " + daysAtFirstVariant + " дней");
+        int daysAtSecondVariant = necessaryWeightLoss / secondWeightLossPerDay;
+        System.out.println("Если спортсмен будет терять за день по 500 грамм, то для похудения ему потребуется " + daysAtSecondVariant + " дней");
+        int daysOnAverage = necessaryWeightLoss / ((firstWeightLossPerDay + secondWeightLossPerDay)/2);
+        System.out.println("В среднем, чтобы сбросить вес, спортсмену понадобится " + daysOnAverage + " дней");
         }
 }
