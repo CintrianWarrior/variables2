3982ea4f92bf005487c899ae5c879640c43579a0
Cintrian <velegoj2@yandex.ru>
2022-09-03 20:52:03 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // Задача 1
         int ant = 1234567;
         System.out.println("Значение переменной ant с типом int равно " + ant);
         byte ball = 50;
@@ -12,5 +13,22 @@ public class Main {
         System.out.println("Значение переменной bugWeight с типом float равно " + bugWeight);
         double bagWeight = 5.14567315;
         System.out.println("Значение переменной bagWeight с типом double равно " + bagWeight);
+        // Задача 2
+        float roadLength = 26.12f;
+        long vocabularySize = 987678965549L;
+        double columnHeight = 2.786;
+        boolean enoughHeight = columnHeight > 3;
+        var hat = 569;
+        short depth = -159;
+        int capacity = 27897;
+        byte manAge = 67;
+        System.out.println(roadLength);
+        System.out.println(vocabularySize);
+        System.out.println(columnHeight);
+        System.out.println(enoughHeight);
+        System.out.println(hat);
+        System.out.println(depth);
+        System.out.println(capacity);
+        System.out.println(manAge);
         }
 }
