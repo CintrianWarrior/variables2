b7d82bf5901139f45d7a54e5d236b4936fdb7d8d
Cintrian <velegoj2@yandex.ru>
2022-09-03 21:23:45 +0300

    Task4

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 3
-        byte firstClass = 23;
-        byte secondClass = 27;
-        byte thirdClass = 30;
-        int studentsNumber = firstClass + secondClass + thirdClass;
-        short paper = 480;
-        int papersPerStudent = paper / studentsNumber;
-        System.out.println("На каждого ученика рассчитано " + papersPerStudent + " листов бумаги");
+        // Задача 4
+        byte productivityIn2Minutes = 16;
+        int productivityPerHour = productivityIn2Minutes * 30;
+        int productivityIn20Minutes = productivityIn2Minutes * 10;
+        System.out.println("За 20 минут машина произвела бутылок " + productivityIn20Minutes + " штук");
+        int productivityPerDay = productivityPerHour * 24;
+        System.out.println("За сутки машина произвела бутылок " + productivityPerDay + " штук");
+        int productivityIn3Days = productivityPerDay * 3;
+        System.out.println("За 3 дня машина произвела бутылок " + productivityIn3Days + " штук");
+        int productivityPerMonth = productivityPerDay * 30;
+        System.out.println("За месяц машина произвела бутылок " + productivityPerMonth + " штук");
         }
 }
