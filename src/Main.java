19a6363a86b46b8722058ffad2e92c1e4c7200ea
Cintrian <velegoj2@yandex.ru>
2022-09-03 23:06:57 +0300

    Task7

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 7
-        short necessaryWeightLoss = 7000;
-        short firstWeightLossPerDay = 250;
-        short secondWeightLossPerDay = 500;
-        int daysAtFirstVariant = necessaryWeightLoss / firstWeightLossPerDay;
-        System.out.println("Если спортсмен будет терять за день по 250 грамм, то для похудения ему потребуется " + daysAtFirstVariant + " дней");
-        int daysAtSecondVariant = necessaryWeightLoss / secondWeightLossPerDay;
-        System.out.println("Если спортсмен будет терять за день по 500 грамм, то для похудения ему потребуется " + daysAtSecondVariant + " дней");
-        int daysOnAverage = necessaryWeightLoss / ((firstWeightLossPerDay + secondWeightLossPerDay)/2);
-        System.out.println("В среднем, чтобы сбросить вес, спортсмену понадобится " + daysOnAverage + " дней");
+        // Задача 8
+        int MashaSalaryPerMonth = 67760;
+        int DenisSalaryPerMonth = 83690;
+        int KristinaSalaryPerMonth = 76230;
+        double newMashaSalaryPerMonth = MashaSalaryPerMonth + MashaSalaryPerMonth * 0.1;
+        double newDenisSalaryPerMonth = DenisSalaryPerMonth + DenisSalaryPerMonth * 0.1;
+        double newKristinaSalaryPerMonth = KristinaSalaryPerMonth + KristinaSalaryPerMonth * 0.1;
+        double differenceMashaSalaryPerYear = newMashaSalaryPerMonth * 12 - MashaSalaryPerMonth * 12;
+        double differenceDenisSalaryPerYear = newDenisSalaryPerMonth * 12 - DenisSalaryPerMonth * 12;
+        double differenceKristinaSalaryPerYear = newKristinaSalaryPerMonth * 12 - KristinaSalaryPerMonth * 12;
+        System.out.println("Маша теперь получает " + newMashaSalaryPerMonth + " рублей. Годовой доход вырос на " + differenceMashaSalaryPerYear + " рублей.");
+        System.out.println("Денис теперь получает " + newDenisSalaryPerMonth + " рублей. Годовой доход вырос на " + differenceDenisSalaryPerYear + " рублей.");
+        System.out.println("Кристина теперь получает " + newKristinaSalaryPerMonth + " рублей. Годовой доход вырос на " + differenceKristinaSalaryPerYear + " рублей.");
         }
 }
