b0270d5cc939c3e323e841f16eabca43b657cb26
Cintrian <velegoj2@yandex.ru>
2022-09-03 21:34:22 +0300

    Task5

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 4
-        byte productivityIn2Minutes = 16;
-        int productivityPerHour = productivityIn2Minutes * 30;
-        int productivityIn20Minutes = productivityIn2Minutes * 10;
-        System.out.println("За 20 минут машина произвела бутылок " + productivityIn20Minutes + " штук");
-        int productivityPerDay = productivityPerHour * 24;
-        System.out.println("За сутки машина произвела бутылок " + productivityPerDay + " штук");
-        int productivityIn3Days = productivityPerDay * 3;
-        System.out.println("За 3 дня машина произвела бутылок " + productivityIn3Days + " штук");
-        int productivityPerMonth = productivityPerDay * 30;
-        System.out.println("За месяц машина произвела бутылок " + productivityPerMonth + " штук");
+        // Задача 5
+        byte paintCans = 120;
+        int classes = paintCans / 6;
+        int whitePaintCans = classes * 2;
+        int brownPaintCans = classes * 4;
+        System.out.println("В школе, где " + classes + " классов, нужно " + whitePaintCans + " банок белой краски и " + brownPaintCans + " банок коричневой краски");
         }
 }
